007ffb80660a6f09b1e1fc34a78b0ac3c18f64fe
James Rogers <jarogers095@gmail.com>
2020-02-05 07:20:24 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
-
+class Book
+  def initialize(book_title)
+    @title = book_title
+  end
+  
+  attr_accessor(:author, :pages, :genre)
+  
+  def turn_page()
+    puts "The book has magically turned its own page!"
+  end
+end
 
